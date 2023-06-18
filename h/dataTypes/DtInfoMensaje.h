7bc0d1bc8c56b763c2e4bddba975ce753469f156
michaelramos95 <mramosit95@gmail.com>
2023-06-18 18:41:40 -0300

    a
    b

--- h/dataTypes/DtInfoMensaje.h
+++ h/dataTypes/DtInfoMensaje.h
@@ -1,5 +1,33 @@
-//Lo hice por dependencias - Juan
+#ifndef DTINFOMENSAJE_H
+#define DTINFOMENSAJE_H
+
+#include <iostream>
+#include <cstdlib>
+#include <list>
+#include <string>
+#include "DTVisto.h"
+
+using namespace std;
 
 class DtInfoMensaje
 {
+    private:
+        string numTel;
+        string nombre;
+        list<DtVisto> vistoPor;
+    public:
+        // Constructor
+        DtInfoMensaje(string numTel, string nombre, list<DtVisto> vistoPor);
+
+        // Getters
+        string getNumTel();
+        string getNombre();
+        list<DtVisto> getVistoPor();
+ 
+        // Destructor
+        ~DtInfoMensaje();
+            
+        //friend ostream &operator<<(ostream &o, DtInfoMensaje &s);
 };
+
+#endif
--- h/dataTypes/DtUsuario.h
+++ h/dataTypes/DtUsuario.h
@@ -1,5 +1,35 @@
-//Lo hice por dependencias - Juan
+#ifndef DTUSUARIO_H
+#define DTUSUARIO_H
+
+#include <iostream>
+#include <string>
+#include "DTFecha.h"
+using namespace std;
 
 class DtUsuario
 {
+    private:
+        string numTel;
+        string nombre;
+        DtFecha fechaRegistro;
+        string imagenPerfil;
+        string descripcion;
+        DtFecha ultimaConexion;
+    public:
+        // Constructor
+        DtUsuario(string numTel, string nombre, DtFecha fechaRegistro, string imagenPerfil, string descripcion, DtFecha ultimaConexion);
+
+        // Getters
+        string getNumTel();
+        string getNombre();
+        DtFecha getFechaRegistro();
+        string getImagenPerfil();
+        string getDescripcion();
+        DtFecha getUltimaConexion();   
+        // Destructor
+        ~DtUsuario();
+            
+        friend ostream &operator<<(ostream &o, DtUsuario &s);
 };
+
+#endif
