9492eb83f9b5d09950d473011c1eeadeb9b44353
MacielFede <federicomaciel.dev@gmail.com>
2023-06-22 19:30:42 -0300

    Add files via upload

---
+++ h/clases/Grupo.h
@@ -0,0 +1,35 @@
+#ifndef GRUPO_H
+#define GRUPO_H
+
+#include "Conversacion.h"
+#include "Participante.h"
+
+class Grupo : public Conversacion
+{
+private:
+    string imagen;
+    DtFecha fechaCreacion;
+    map<string, Participante *> Participantes;
+public:
+    DtConversacion getDataConversacion();
+
+    list<DtMensaje> buscarMensajes();
+
+    DtInfoMensaje informacionMensaje(string idMensaje);
+
+    void asignarAConversacion(Mensaje* m);
+    
+    Grupo(); // Lo dejo vacio porque no se bien como seria el caso de uso de crear un grupo y como se le irian pasando los parametros
+
+    //No creo setters porque no deberiamos querer cambiar nada de los grupos
+
+    string getImagen();
+
+    DtFecha getFechaCreacion();
+
+    //list<DtParticipantes> getParticipantes(); No se si es necesario pero dejo la firma por las dudas
+
+    ~Grupo();
+};
+
+#endif
